4948b45aefb30843f767d894eb823329e9c5c4b8
Natanael Arndt <arndtn@gmail.com>
2023-12-07 20:41:53 +0100

    Assign methods to __all__

--- databusclient/__init__.py
+++ databusclient/__init__.py
@@ -1,5 +1,7 @@
 from databusclient import cli
 from databusclient.client import create_dataset, deploy, create_distribution
 
+__all__ = ["create_dataset", "deploy", "create_distribution"]
+
 def run():
     cli.app()
